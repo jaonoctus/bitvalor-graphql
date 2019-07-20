b9a3839445e1c70d107c0e2d5a5067bf001e1636
Joao Dias <joaodias@noctus.org>
2019-07-20 14:04:48 -0300

    Redis config

--- src/cache.js
+++ src/cache.js
@@ -1,9 +1,16 @@
 const cacheManager = require('cache-manager')
 const redisStore = require('cache-manager-redis')
 
+const redisConfig = {
+    host: process.env.REDIS_HOST || 'localhost',
+    port: process.env.REDIS_PORT || 6379,
+    user: process.env.REDIS_USER || '',
+    auth_pass: process.env.REDIS_PASS || ''
+}
+
 const cache = cacheManager.caching({
     store: redisStore,
-    auth_pass: '',
+    ...redisConfig,
     db: 0,
     ttl: 600
 })
